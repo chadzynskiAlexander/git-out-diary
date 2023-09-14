6ecfda6ab6b828a038493337194841bbb9e289c5
WordlessSafe1 <94203733+WordlessSafe1@users.noreply.github.com>
2023-09-14 14:33:51 -0500

    Add typehinting, create add function

--- diary.py
+++ diary.py
@@ -1,8 +1,13 @@
+from typing import Tuple
+
 class Diary:
     def __intit__(self):
-        self.entries = []
+        self.entries: Tuple[str,str] = []
 
     @property
-    def entries(self):
+    def entries(self) -> Tuple[str,str]:
         return self.entries
-    pass
+
+    def add(self, date, body) -> None:
+        self.entries.append(Tuple(date,body))
+
